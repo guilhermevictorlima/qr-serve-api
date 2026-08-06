07e3b8d9e89254012f21eb9ba2fb39dda8f802e5
Guilherme Lima <guilhermevictorrocha@gmail.com>
2026-08-06 16:16:50 -0300

    refactor: rename package

--- src/main/java/br/com/qrserve/QrServeApplication.java
+++ src/main/java/br/com/qrserve/QrServeApplication.java
@@ -1,4 +1,4 @@
-package com.rochacore.qrserve;
+package br.com.qrserve;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/br/com/qrserve/QrServeApplicationTests.java
+++ src/test/java/br/com/qrserve/QrServeApplicationTests.java
@@ -1,4 +1,4 @@
-package com.rochacore.qrserve;
+package br.com.qrserve;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
