4ab51209749bcf21ef34b145f99880bb7b819e6c
Guilherme Lima <guilhermevictorrocha@gmail.com>
2026-08-27 14:37:22 -0300

    chore(0.1.0): insere log na entrada do endpoint

--- src/main/java/br/com/qrserve/controllers/SessaoMesaController.java
+++ src/main/java/br/com/qrserve/controllers/SessaoMesaController.java
@@ -4,6 +4,8 @@ import br.com.qrserve.models.dto.form.AcessarSessaoMesaForm;
 import br.com.qrserve.models.dto.response.AcessarSessaoMesaResponse;
 import br.com.qrserve.services.SessaoMesaService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/session")
 public class SessaoMesaController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SessaoMesaController.class);
 
     private final SessaoMesaService service;
 
@@ -21,6 +24,7 @@ public class SessaoMesaController {
 
     @PostMapping
     public AcessarSessaoMesaResponse acessar(@Valid @RequestBody AcessarSessaoMesaForm form) {
+        LOGGER.info("[POST]/session: " + form.toString());
         return service.acessar(form);
     }
 
