300db64f5041f3944b29405cc78f4b0c75d117f0
Guilherme Lima <guilhermevictorrocha@gmail.com>
2026-08-27 14:36:28 -0300

    fix(0.1.0): corrige mapeamento do controller

--- src/main/java/br/com/qrserve/controllers/SessaoMesaController.java
+++ src/main/java/br/com/qrserve/controllers/SessaoMesaController.java
@@ -6,9 +6,11 @@ import br.com.qrserve.services.SessaoMesaService;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/session")
+@RestController
+@RequestMapping("/session")
 public class SessaoMesaController {
 
     private final SessaoMesaService service;
