fcba99286a50f5b9cf20d0b9ae1d2dd1a88bae3f
Guilherme Lima <guilhermevictorrocha@gmail.com>
2026-08-28 14:11:47 -0300

    chore(0.1.0): define regra de horário da aplicação

---
+++ src/main/java/br/com/qrserve/config/TimeConfig.java
@@ -0,0 +1,17 @@
+package br.com.qrserve.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Clock;
+import java.time.ZoneId;
+
+@Configuration
+public class TimeConfig {
+
+    @Bean
+    public Clock clock() {
+        return Clock.system(ZoneId.of("America/Sao_Paulo"));
+    }
+
+}
