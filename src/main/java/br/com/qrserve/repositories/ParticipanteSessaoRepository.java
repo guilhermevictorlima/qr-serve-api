3b72dbd173e3d53bdbbb724e1420172eb6fb1089
Guilherme Lima <guilhermevictorrocha@gmail.com>
2026-08-28 17:00:59 -0300

    feat(0.1.0): expõe endpoint que lista participantes

--- src/main/java/br/com/qrserve/repositories/ParticipanteSessaoRepository.java
+++ src/main/java/br/com/qrserve/repositories/ParticipanteSessaoRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,5 +15,10 @@ public interface ParticipanteSessaoRepository extends JpaRepository<Participante
             select * from participante_sessao where token = ?1
             """, nativeQuery = true)
     Optional<ParticipanteSessao> obterParticipantePorToken(String tokenUsuario);
+
+    @Query(value = """
+            select * from participante_sessao where sessao_id = ?1
+            """, nativeQuery = true)
+    List<ParticipanteSessao> listarParticipantes(Integer sessaoId);
 
 }
