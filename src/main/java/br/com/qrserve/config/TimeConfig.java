a26585b5f7026cc331530ff54771ddac05ff7bd0
Guilherme Lima <guilhermevictorrocha@gmail.com>
2026-08-28 22:58:24 -0300

    chore(0.1.0): cria métodos auxiliares de data na classe TimeConfig

--- src/main/java/br/com/qrserve/config/TimeConfig.java
+++ src/main/java/br/com/qrserve/config/TimeConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.Clock;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 
 @Configuration
@@ -12,6 +14,14 @@ public class TimeConfig {
     @Bean
     public Clock clock() {
         return Clock.system(ZoneId.of("America/Sao_Paulo"));
+    }
+
+    public LocalDate dataAtual() {
+        return LocalDate.now(clock());
+    }
+
+    public LocalDateTime dataHoraAtual() {
+        return LocalDateTime.now(clock());
     }
 
 }
